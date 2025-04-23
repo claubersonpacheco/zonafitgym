f369db694d667508e5d84b54384544bd728b4a79
caubinho <caubinho@gmail.com>
2025-04-23 22:50:48 +0200

    Metodo guardar

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
+from cliente import Cliente
 from cliente_dao import ClienteDAO
+from cliente_forma import ClienteForma
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'llave_secreta_123'
 
 titulo_app = 'Zona Fit (GYM)'
 
@@ -13,8 +17,28 @@ def index():
     #recuperamos los clientes de la bd
 
     cliente_db = ClienteDAO.seleccionar()
+    #Creamos un objeto de formulario vacio
+    cliente = Cliente()
 
-    return render_template('index.html', titulo=titulo_app, clientes=cliente_db)
+    cliente_forma = ClienteForma(obj=cliente)
+
+    return render_template('index.html', titulo=titulo_app, clientes=cliente_db, forma=cliente_forma)
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    #creamos los objetos de cliente inicialmente objetos vacios
+    cliente = Cliente()
+    cliente_forma = ClienteForma(obj=cliente)
+
+    if cliente_forma.validate_on_submit():
+        # llenamos el objeto cliente con los valores del formulario
+        cliente_forma.populate_obj(cliente)
+
+        # Guardamos el nuevo cliente en la bd
+        ClienteDAO.insertar(cliente)
+    # redirecionar a la pagina de inicio
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
